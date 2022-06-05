6d9a45609b2c28b90719856dc49bc62fc7e8c572
Lina Grundvall <lina.grundvall@learnet.se>
2022-06-05 16:09:06 +0200

    Escape?

--- VTC/Default.aspx.cs
+++ VTC/Default.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -28,7 +29,11 @@ namespace VTC
                          UploadedFile.PostedFile.ContentLength + " kb<br>" +
                          "Content type: " + UploadedFile.PostedFile.ContentType;
 
-                    Session["uploadedFile"] = "~/uploads/" + UploadedFile.FileName;
+                    Session["uploadedFilePath"] = "~/uploads/" + UploadedFile.FileName;
+                    Session["uploadedFileName"] = UploadedFile.FileName;
+                    Session["pathUploads"] = Server.MapPath("~/uploads/");
+                    Session["pathConverted"] = Server.MapPath("~/converted/");
+
                 }
                 catch (Exception ex)
                 {
@@ -46,8 +51,44 @@ namespace VTC
                 try
                 {
                     var fileToConvert = Session["uploadedFile"];
-                    Console.WriteLine("Hej");
 
+                    // Start the child process.
+                    Process p = new Process();
+                    // Redirect the output stream of the child process.
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardError = true;
+                    p.StartInfo.FileName = "cmd.exe";
+
+                    string replaceWith = "\\";
+                    replaceWith += "\u0022";
+                    Session["pathUploads"] = Session["pathUploads"].ToString().Replace("\\", replaceWith);
+
+
+                    string Arguments = "/C ffmpeg -i " + Session["pathUploads"] + "traffic.mp4" + Session["pathConverted"] + "\\traffic.avi";
+                    //"/C ffmpeg -i " + "source\\repos\\VTC\\VTC Form\\VTC\\uploads\\" +
+                    //"uploadedFileName" + " source\\repos\\VTC\\VTC Form\\VTC\\converted";
+
+                    FileConvertedLabel.Text = Arguments;
+
+
+                    p.StartInfo.Arguments = Arguments;
+                    //"/C ffmpeg -i videos\\copy.mp4 converted\\copy.avi";
+                    p.Start();
+                    p.WaitForExit();
+                    //p.StartInfo.Arguments = "/C Del videos\\copy.mp4";
+                    //p.Start();
+                    //p.WaitForExit();
+
+
+                    // Read the output stream first and then wait.
+                    string output = p.StandardOutput.ReadToEnd();
+                    FileConvertedLabel.Text = output;
+
+
+                    FileConvertedLabel.Text = "You have converted a file.";
+                    string output2 = p.StandardError.ReadToEnd();
+                    FileConvertedLabel.Text = output2;
 
 
                 }
