aef7c7a182b02bc84e54a95d0f2a199e17a0e7e9
Lina Grundvall <lina.grundvall@learnet.se>
2022-06-05 09:47:09 +0200

    Add project files.

---
+++ VTC/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="VTC._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Video Transcoder</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+                <asp:FileUpload ID="FileUpload1" runat="server" Width="303px" />
+                <br />
+                <asp:Button ID="UploadButton" runat="server" 
+                    OnClick="Upload" Text="Upload file" />
+                <br/>
+                <asp:Label ID="FileUploadedLabel" runat="server" />
+            <br/>
+        <asp:Button ID="ConvertButton" runat="server" 
+                    OnClick="Convert" Text="Convert" />
+        <br/>
+        </div>
+    </div>
+
+
+</asp:Content>
---
+++ VTC/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VTC
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+                try
+                {
+                    FileUpload1.SaveAs(Server.MapPath("~/uploads/") +
+                         FileUpload1.FileName);
+                    FileUploadedLabel.Text = "File name: " +
+                         FileUpload1.PostedFile.FileName + "<br>" +
+                         FileUpload1.PostedFile.ContentLength + " kb<br>" +
+                         "Content type: " + FileUpload1.PostedFile.ContentType;
+                }
+                catch (Exception ex)
+                {
+                    FileUploadedLabel.Text = "ERROR: " + ex.Message.ToString();
+                }
+            else
+            {
+                FileUploadedLabel.Text = "You have not specified a file.";
+            }
+        }
+
+        protected void Convert(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hello Converter");
+        }
+
+    }
+}
